612f4f2181e3651714a8392f40b68f125e5b3170
Jason Nutt <jnutt367@gmail.com>
2021-03-12 12:02:10 -0500

    created variables for all imgs in js folder to do something with them

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,7 @@
-console.log('this file is connected mr Jason!')
+let smImg = document.getElementById('sm-img');
+let medImg = document.getElementById('med-img');
+let lgImg = document.getElementById('lg-img');
+
+
+
+console.log(medImg, smImg, lgImg);
